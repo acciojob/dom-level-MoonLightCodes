5c4157749486fef333cf68703c471fcbecb27c14
PoornachanderReddy <poornachander295@gmail.com>
2025-04-20 23:40:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let items = document.querySelectorAll('li');
+items.forEach((e,i)=>{
+	if(e.getAttribute("ID")==="level"){
+		alert("The level of the element is: "+(i));
+	}
+})
